30de684fd33caa3dcb18ae8128e58e06ec1ac652
MaximMurzaev <m.murzaev@mail.ru>
2024-10-12 12:38:28 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+
+#task1
+print("Helo world")
+
+#tast2 Арифметика
+#1st program
+print(9**0.5*5)
+
+#2nd program Логика
+print(9.99>9.98 and 1000!=1000.1)
+
+#3rd program Школьная задача
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+#4th program Первый после точки
+a = float('123.456')*10
+print(int(a%10))
