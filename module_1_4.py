4a49d6f0be314a48e798ff6b3f9b176c8fed2e06
MaximMurzaev <m.murzaev@mail.ru>
2024-10-13 16:16:00 +0700

    новая версия

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+# my_string = input('Введите произвольный текст: ')
+# print(len(my_string))
+# print(my_string.upper())
+# print(my_string.lower())
+# print(my_string.replace(' ',''))
+# print(my_string[0])
+# print(my_string[-1])
+
+name = input("Введите ваше имя: ")
+print('Здравствуйте ' + name + '!')
+yar = int(input('В каком году вы родились? '))
+print('Сегодня вам ', 2024 - yar, 'лет')
