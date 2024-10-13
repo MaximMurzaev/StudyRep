76ddc12e083d39ecc2fb4955d652b5b3e8fb1b3c
MaximMurzaev <m.murzaev@mail.ru>
2024-10-13 15:56:05 +0700

    add file

---
+++ module_1_3.py
@@ -0,0 +1,21 @@
+# #1
+# name = 'Maksim'
+# print(name)
+# age = 40
+# print(age)
+# age = age + 1
+# print(age)
+# is_student = True
+# print(is_student)
+# #2
+# complete_home_work = 12
+# spent_time = 1.5
+# course_name = 'Python'
+# one_home_work =  spent_time / complete_home_work
+# print('Курс: ' + course_name + ', всего задач:', complete_home_work, ', затрачено часов:', spent_time, ', среднее время выполнения:', one_home_work)
+# #3
+# example = 'Альбинос'
+# print(example[0])
+# print(example[4:])
+# print(example[::-1])
+# print(example[::2])
