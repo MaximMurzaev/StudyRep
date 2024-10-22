ed28ebc299030df090edf28dae51cc262d2c44c3
MaximMurzaev <m.murzaev@mail.ru>
2024-10-22 19:57:29 +0700

    новая версия

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+#
+# #task1
+# print("Helo world")
+#
+# #tast2 Арифметика
+# #1st program
+# print(9**0.5*5)
+#
+# #2nd program Логика
+# print(9.99>9.98 and 1000!=1000.1)
+#
+# #3rd program Школьная задача
+# print(2*2+2)
+# print(2*(2+2))
+# print(2*2+2==2*(2+2))
+#
+# #4th program Первый после точки
+# a = float('123.456')*10
+# print(int(a%10))
 
-#task1
-print("Helo world")
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
 
-#tast2 Арифметика
-#1st program
-print(9**0.5*5)
-
-#2nd program Логика
-print(9.99>9.98 and 1000!=1000.1)
-
-#3rd program Школьная задача
-print(2*2+2)
-print(2*(2+2))
-print(2*2+2==2*(2+2))
-
-#4th program Первый после точки
-a = float('123.456')*10
-print(int(a%10))
