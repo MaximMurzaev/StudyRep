2c4a80a818ac9e85e0abbe884c491da460989e0c
MaximMurzaev <m.murzaev@mail.ru>
2024-10-22 21:13:05 +0700

    новая версия

--- module_2_2.py
+++ module_2_2.py
@@ -1,7 +1,7 @@
 first = int(input('Введите первое число '))
 second = int(input('Введите второе число '))
 third = int(input('Введите третье число '))
-num_set = set([first, second, third])
+num_set = {first, second, third}
 if len(num_set) == 1:
     print(3)
 elif len(num_set) == 2:
