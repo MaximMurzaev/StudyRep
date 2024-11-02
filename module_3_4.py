5570dfbeff35f69250bab4abf74c263a42171233
MaximMurzaev <m.murzaev@mail.ru>
2024-11-02 09:17:44 +0700

    новая версия

--- module_3_4.py
+++ module_3_4.py
@@ -2,11 +2,11 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for word in other_words:
-        if root_word in word:
+        if root_word.lower() in word.lower():
             same_words.append(word)
     return same_words
 
-result = single_root_words('кол', 'колокол', "сабля", "ледокол", "калькулятор")
+result = single_root_words('кол', 'коЛокол', "сабля", "ледоКол", "калькулятор")
 result_1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 print(result)
 print(result_1)
