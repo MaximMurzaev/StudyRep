9362d80d4657e59d9e5cc3ac1b98d729d94ce318
MaximMurzaev <m.murzaev@mail.ru>
2024-11-09 08:57:07 +0700

    новая версия

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+
+from  fake_math import devide as fake_dev
+from true_math import devide as true_dev
+
+result1 = fake_dev(33, 3)
+result2 = fake_dev(1, 0)
+result3 = true_dev(50, 7)
+result4 = true_dev(5, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
