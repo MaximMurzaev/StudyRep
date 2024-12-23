44c0b9a265618189ac92f1d023d931c2e0984e85
MaximMurzaev <m.murzaev@mail.ru>
2024-12-23 17:19:21 +0700

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,11 @@
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(i[0]) - len(i[1]) if len(i[0]) > len(i[1]) else len(i[1]) - len(i[0]) for i in zip(first, second)
+                if len(i[0]) != len(i[1]))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
